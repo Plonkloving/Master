65af70d8b72a14d9f1a09746426a3d1eb954cebe
Plonkloving <102906830+Plonkloving@users.noreply.github.com>
2022-06-11 21:35:47 +0800

    Add files via upload
    聊天小程序

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Server_正式版_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(95, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(322, 37);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "10.178.23.104";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(584, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开始监听";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(95, 82);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(945, 190);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(95, 350);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(945, 201);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(459, 23);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 36);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "50000";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(703, 592);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "选择";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(703, 653);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "发消息";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(873, 653);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "震动";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(95, 592);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(580, 25);
+            this.textBox5.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(873, 592);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "发送文件";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(726, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(217, 23);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1368, 747);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_正式版_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //创建一个负责监听ip的socket
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//需要三个参数：表示我的IP版本是v4  socket类型  你传输数据使用哪种协议
+                                                                                                            //但此时我电脑的IP地址还未绑定到这个socket上，它现在作用是一个信号塔，电话还未连到移动的平台上
+                                                                                                            //接下来，我要做的是把这个信号塔和电话绑定
+
+
+                //既然要绑定，那么就必须保证有电话和电话号码
+                //保证服务器有电话
+                IPAddress ip = IPAddress.Any;  //new IPAddress(long.Parse(textBox1.Text));
+                                               //保证电话能被打，就是有电话号码
+                IPEndPoint iep = new IPEndPoint(ip, int.Parse(textBox2.Text));
+
+                //把这个电话和电话号码上传到电话公司，让他们帮我们把电话 号码与信号塔绑定，以让信号塔监视电话
+                socket.Bind(iep);
+                //告诉大家我已经把我要的监听系统和电话绑定好，服务器已经练好了
+                ShowMsg("监听成功");
+
+
+                //下面就可以去监视是否有客户端请求来连接我的IP地址和端口号，如果有客户端想连接，那么服务器的信号塔（监视socket）就会把这个信号传给对方的手机，也就是建立一个通信机制
+                //但是我在接收这个信号的时候，发现信号有点多，怎么办？让他们去排个队
+                //设置个监听队列
+                socket.Listen(10);
+
+
+                Thread th = new Thread(Listen);
+
+                th.IsBackground = true;
+                th.Start(socket);
+            }
+            catch
+            { }
+        }
+
+        /// <summary>
+        /// 只是为了展示消息，把最终的监听和连接成果反馈
+        /// </summary>
+        /// <param name="str"></param>
+        void ShowMsg(string str)
+        {
+            textBox3.AppendText(str + "\r\n");
+        }
+
+        //存储连接过的客户端的IP和端口号
+        Dictionary<string, Socket> dicSocket = new Dictionary<string, Socket>();
+        //负责通信的socket
+        Socket socketSend;
+        
+        /// <summary>
+        /// 接收信号，建立一个socket
+        /// </summary>
+        /// <param name="socket"></param>
+        void Listen(Object o)
+        {
+            Socket socket = o as Socket;
+            while (true)
+            {
+                try
+                {
+                    //信号塔接收到信号之后，建了一个通信的工具。负责通信的socket
+                     socketSend = socket.Accept();
+                    //将远程连接的IP和端口号存入集合
+                    dicSocket.Add(socketSend.RemoteEndPoint.ToString(), socketSend);
+                    //将远程IP存入下拉框中
+                    comboBox1.Items.Add(socketSend.RemoteEndPoint.ToString());
+                    ShowMsg(socketSend.RemoteEndPoint.ToString() + "\t连接成功");
+                    
+                    //建立一个新的线程，负责在客户端连接成功后，去接收客户端发来的消息
+                    Thread th = new Thread(Send);
+                    th.IsBackground = true;
+                    th.Start(socketSend);
+                }
+                catch
+                { }
+
+            }
+        }
+        
+
+        
+       
+
+        /// <summary>
+        /// 不停的接收客户端发来的消息，并把这个消息反应在我的消息框中
+        /// </summary>
+        /// <param name="o"></param>
+        void Send(Object o)
+        {
+            while (true)
+            {
+                try
+                {
+                    Socket socketSend = o as Socket;
+                    //客户端连接成功后，服务器开始接收客户端发来的消息
+                    byte[] buffer = new byte[1024 * 1024 * 5];
+                    int r = socketSend.Receive(buffer);
+                    if (r == 0)
+                    {
+                        break;
+                    }
+                    string str = Encoding.UTF8.GetString(buffer, 0, r);
+                    string str2 = socketSend.RemoteEndPoint.ToString() + "\t" + str;
+                    ShowMsg(str2);
+                }
+                catch
+                { }
+            }
+
+        }
+
+
+        /// <summary>
+        /// 窗体加载的时候取消对跨线程的访问
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        /// <summary>
+        /// 点击它的时候，服务器会把第二个文本框中的消息发给客户端
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string str = textBox4.Text;
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(str);
+            List<byte> list = new List<byte>();
+            list.Add(0);
+            list.AddRange(buffer);
+            byte[] bytes = list.ToArray();
+            //socketSend.Send(buffer);
+            
+            //获得用户在下拉框中选中的IP地址与端口号
+            string ip = comboBox1.SelectedItem.ToString();
+            dicSocket[ip].Send(bytes);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "所有文件|*.*";
+            ofd.InitialDirectory = @"F:\";
+            ofd.Multiselect = true;
+            ofd.ShowDialog();
+            string path = ofd.FileName;
+            textBox5.Text = path;
+
+
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(textBox5.Text, FileMode.OpenOrCreate, FileAccess.Read))
+                {
+                    byte[] buffer = new byte[1024 * 1024 * 5];
+                    int r = fs.Read(buffer, 0, buffer.Length);
+                    List<byte> list = new List<byte>();
+                    list.Add(1);
+                    list.AddRange(buffer);
+                    byte[] newBuffer = list.ToArray();
+
+                    dicSocket[comboBox1.SelectedItem.ToString()].Send(newBuffer, 0, r+1, SocketFlags.None);
+                }
+            }
+            catch
+            { }
+        }
+
+        /// <summary>
+        /// 发送震动
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            byte[] buffer = new byte[1];
+            buffer[0] = 2;
+            dicSocket[comboBox1.SelectedItem.ToString()].Send(buffer, 0, 1, SocketFlags.None);
+        }
+    }
+}
